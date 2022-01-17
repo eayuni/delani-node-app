cb609ad94ce8e50849f67b121e17614b77da7b12
Eunice Otieno <otieunice@gmail.com>
2022-01-17 09:54:35 +0300

    added http server

---
+++ server.js
@@ -0,0 +1,9 @@
+// Craeting a basic http server
+
+let http= require('http');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+   }).listen(8000);
